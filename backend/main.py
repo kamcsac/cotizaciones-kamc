da77a03efe7e66cb12dcf11953c6b9ba0a8f4bb4
kamcsac <bcastillo.minaya@gmail.com>
2026-01-19 20:12:21 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# USUARIO FIJO (por ahora)
+USUARIO = "BCASTILLO"
+PASSWORD = "KAMCSAC"
+
 @app.get("/")
 def inicio():
     return {"mensaje": "API KAMC funcionando"}
+
+@app.post("/login")
+def login(usuario: str, password: str):
+    if usuario == USUARIO and password == PASSWORD:
+        return {"usuario_id": 1, "rol": "ADMIN"}
+    return {"error": "Credenciales incorrectas"}
+
+@app.post("/cotizacion")
+def guardar_cotizacion(data: dict):
+    return {"mensaje": "Cotización guardada"}
