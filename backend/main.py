78823c41d94f5e55b63d7d3691922e5365592897
kamcsac <bcastillo.minaya@gmail.com>
2026-01-19 19:37:56 -0500

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Cotizaciones KAMC S.A.C.")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API KAMC funcionando"}
